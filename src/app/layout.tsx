ae9f3c0b70c8be4203684acfd81d56461d072523
conner <conner@localhost.localdomain>
2025-02-09 23:30:12 +0530

    [INIT] Hono.js Setup + Tanstack Query Setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,25 +3,26 @@ import { Inter } from "next/font/google";
 import { cn } from "@/lib/utils";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
+import { QueryProvider } from "@/components";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	title: "Jira",
-	description: "Project management app",
+  title: "Jira",
+  description: "Project management app",
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en">
-			<body className={cn(inter.className, "antialiased min-h-screen")}>
-					<Toaster />
-					{children}
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body className={cn(inter.className, "antialiased min-h-screen")}>
+        <Toaster />
+        <QueryProvider>{children}</QueryProvider>
+      </body>
+    </html>
+  );
 }
