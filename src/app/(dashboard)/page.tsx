6e7210961c601b104eab986ef78152fa0c738296
conner <conner@localhost.localdomain>
2025-02-13 00:24:13 +0530

    [misc.] minor changes

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,14 +1,9 @@
 import { redirect } from "next/navigation";
 
-import { UserButton } from "@/features";
 import { getCurrent } from "@/features/actions";
 
 export default async function Home() {
   const user = await getCurrent();
   if (!user) redirect("/sign-in");
-  return (
-    <div className="">
-      <UserButton />
-    </div>
-  );
+  return <div className=""></div>;
 }
--- src/features/auth/components/index.ts
+++ src/features/auth/components/index.ts
@@ -1,3 +1,3 @@
 export { SignInCard } from "./sign-in-card";
 export { SignUpCard } from "./sign-up-card";
-export { UserButton } from "./user-button";
+export { UserButton } from "../../../components/shared/user-button";
