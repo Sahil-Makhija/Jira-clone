f0ba6e2915286e8cffc6dd9c5cbc7091f10464f2
Sahil-Makhija <sahilmakhija667@gmail.com>
2025-03-11 11:54:22 +0530

    [New] Workspace Create Page

---
+++ src/app/(standalone)/workspaces/create/page.tsx
@@ -0,0 +1,18 @@
+import { redirect } from "next/navigation";
+
+import { CreateWorkspaceForm } from "@/features";
+import { getCurrent } from "@/features/auth/actions";
+
+const WorkspaceCreatePage = async () => {
+  const user = await getCurrent();
+
+  if (!user) redirect("/sign-in");
+
+  return (
+    <div className="w-full lg:max-w-xl">
+      <CreateWorkspaceForm />
+    </div>
+  );
+};
+
+export default WorkspaceCreatePage;
